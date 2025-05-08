7e07d7f3539f1c4d2c50a0d9dc95d086ef380be5
SeuNomeNoGitHub <seuemail@exemplo.com>
2025-05-08 15:32:05 -0300

    Initial commit

---
+++ src/VerificarIdade.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VerificarIdade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade < 0) {
+            System.out.println("Idade inválida. Informe um número positivo. ");
+        } else if (idade >= 18) {
+            System.out.println("Você é maior de idade. ");
+        } else {
+            System.out.println("Você não é maior de idade. ");
+        }
+        scanner.close();
+    }
+}
+
+
+
---
+++ src/notasAluno.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+ class notasAluno {
+ public static void main(String[] args ) {
+
+  Scanner scanner = new Scanner(System.in);
+
+ System.out.println("Digite a sua nota na A1: ");
+  int nota1 = scanner.nextInt();
+
+ System.out.println("Digite sua nota na A2: ");
+ int nota2 = scanner.nextInt();
+
+ System.out.println("Digite sua nota na A3: ");
+ int nota3 = scanner.nextInt();
+
+ int soma = nota1 + nota2 + nota3;
+
+ if (soma <= 0) {
+  System.out.println("Número inválido. Digite um número. ");
+ }
+  else if (soma >= 70) {
+   System.out.println("APROVADO(a) ");
+ }
+  else if (soma >= 40 && soma < 70) {
+   System.out.println("DEPENDÊNCIA ");
+  }
+   else if (soma <= 40) {
+    System.out.println("REPROVADO(a) ");
+  }
+
+  scanner.close();
+
+
+
+
+
+
+  }
+ }
